ce2cd336f62d1f2b12c346f57d0e3f1928ac6b8b
mikearbib <arbib.michael@gmail.com>
2017-07-18 21:17:11 +0200

    update class to className in Navbar

--- app/src/components/Navbar.js
+++ app/src/components/Navbar.js
@@ -3,18 +3,18 @@ import React from 'react';
 export default class Navbar extends React.Component {
     render() {
       return (
-        <nav class="navbar navbar-default" id="myNavbar" data-spy="affix" data-offset-top="760">
+        <nav className="navbar navbar-default" id="myNavbar" data-spy="affix" data-offset-top="760">
           {/* bouton menu mobile */}
-          <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="nav-menu-text">MENU</span>
+          <div className="navbar-header">
+            <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+              <span className="sr-only">Toggle navigation</span>
+              <span className="nav-menu-text">MENU</span>
             </button>
           </div>
           {/* contenu du bouton quand affichage mobile */}
-          <div class="collapse navbar-collapse navbar-ex1-collapse">
-            <ul class="nav navbar-nav">
-              <li class="active"><a href="#profile">À propos de moi</a></li>
+          <div className="collapse navbar-collapse navbar-ex1-collapse">
+            <ul className="nav navbar-nav">
+              <li className="active"><a href="#profile">À propos de moi</a></li>
               <li><a href="#experiences">Expériences</a></li>
               <li><a href="#abilities">Compétences</a></li>
               <li><a href="#projects">Projets</a></li>
